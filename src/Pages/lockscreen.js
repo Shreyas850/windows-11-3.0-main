00498e52fcd36036aa6fdc94ce9ca794c4388b95
Khush <shreyasms49@gmail.com>
2025-07-16 20:13:31 +0530

    Refactor Lockscreen component by removing mobile detection logic and related state

--- src/Pages/lockscreen.js
+++ src/Pages/lockscreen.js
@@ -1,11 +1,8 @@
 import React, { useState, useCallback } from "react";
 import Login from "../components/user/Login";
 import Slider from "../components/utilities/Slider";
-import MobileDetection from "../components/utilities/MobileDetection";
 
 function Lockscreen() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
 
   // Use useCallback to memoize the function and avoid unnecessary re-renders
   const toggleMenu = useCallback(() => {
@@ -13,25 +10,15 @@ function Lockscreen() {
   }, []);
 
   // Callback for mobile detection
-  const handleMobileDetection = useCallback((mobile) => {
-    setIsMobile(mobile);
   }, []);
 
   // Conditional rendering for mobile detection
-  if (isMobile) {
-    return (
-      <div className="bg-black w-full h-screen text-center text-3xl px-7 overflow-hidden flex flex-col justify-center items-center">
-        😔
-        <br />
-        Sorry, this app is not supported on mobile devices{" "}
-        <div className="font-bold">YET.</div> 🙏
       </div>
     );
   }
 
   return (
     <>
-      {!isMobile && <MobileDetection onDetectMobile={handleMobileDetection} />}
 
       <div
         className="absolute bg-black h-screen w-full blur-sm"
